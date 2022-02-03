78b9466e151dd629b4c509c6f90850efe06bbe65
popty <86563042+popty@users.noreply.github.com>
2022-02-03 18:16:28 +0530

    Time: 8 ms (91.71%), Space: 7.7 MB (68.09%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-         vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+        vector<int> map(128,0);
+        for(auto c: t){
+            map[c]++;
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        
+        int begin = 0, end = 0, len = INT_MAX, head=0;
+        int count = t.size();
+        while(end < s.size()){
+            if(map[s[end++]]-->0)count--;
+            
+            while(count==0){
+                if(len > end - begin) len = end - (head=begin);
+                if(map[s[begin++]]++==0)count++;
+            }
+        }
+        
+        return len==INT_MAX?"":s.substr(head, len);
         
     }
 };
