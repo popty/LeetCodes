d365bd4d3cbdab7f1d4d4603fe6a6f8eafc0f725
popty <86563042+popty@users.noreply.github.com>
2022-02-15 13:41:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (72.33%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,14 +1,16 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        triangle = []
-        for row_num in range(numRows):
-            row = [None for _ in range(row_num+1)]
-            row[0], row[-1] = 1, 1
-            
-            for j in range(1, len(row)-1):
-                row[j] = triangle[row_num-1][j-1]+triangle[row_num-1][j]
-                
-            triangle.append(row)
-            
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        for(int i = 0; i<numRows; i++){
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
+            for(int j = 1; j<i; j++ ){
+                res[i][j] = res[i-1][j-1]+res[i-1][j];
+            }
+        }
+       
         
-        return triangle
+        return res;
+    }
+};
