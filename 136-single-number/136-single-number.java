533f6ac843801502bcb79bbe1e10f8d43719a3e2
popty <86563042+popty@users.noreply.github.com>
2022-02-15 12:26:43 +0530

    Time: 14 ms (24.53%), Space: 54.7 MB (5.21%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        
+        for(int i: nums){
+            if(map.get(i)!=2)
+                return i;
+        }
+        
+        return -1;
+    }
+}
