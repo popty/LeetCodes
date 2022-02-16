3544b37a5eaf2f0e56a7f7b07cc93728473c0bea
popty <86563042+popty@users.noreply.github.com>
2022-02-17 00:32:48 +0530

    Time: 1 ms (88.43%), Space: 48.1 MB (9.43%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,20 +1,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       
-        int slow = 0, fast = 0;
-        
-        while(fast < nums.length && slow < nums.length-1){
-            
-            while(fast < nums.length && nums[slow] == nums[fast])fast++;
-            
-            if(fast == nums.length) break;
-            
-            nums[slow+1] = nums[fast];
-            slow++;
-            fast++;
-        }
-        
-        return slow+1;
-        
+        int i = 0;
+        for (int n : nums)
+            if (i == 0 || n > nums[i-1])
+                nums[i++] = n;
+        return i;
     }
 }
