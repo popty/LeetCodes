bfbb77774a5e9194f3c990a111d1bd35313aede7
popty <86563042+popty@users.noreply.github.com>
2022-02-18 09:33:09 +0530

    Time: 3 ms (89.80%), Space: 42.9 MB (39.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,12 +1,13 @@
 class Solution {
+    int[] candidates;
     
     protected void backtrack(
         int remain,
         LinkedList<Integer> comb, 
-        int start, int[] candidates,
+        int start, 
         List<List<Integer>> results){
         if(remain == 0){
-            results.add(new ArrayList<Integer>(comb));
+            results.add(new ArrayList<>(comb));
             return;
         }
         
@@ -14,16 +15,17 @@ class Solution {
         
         for(int i = start; i< candidates.length; i++){
             comb.add(candidates[i]);
-            this.backtrack(remain-candidates[i], comb, i, candidates, results);
+            backtrack(remain-candidates[i], comb, i, results);
             comb.removeLast();
         }
     }
     
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.candidates = candidates;
         List<List<Integer>> results = new ArrayList<>();
         LinkedList<Integer> comb = new LinkedList<Integer>();
-        this.backtrack(target, comb, 0, candidates, results);
+        backtrack(target, comb, 0, results);
         return results;
     }
 }
