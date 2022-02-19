ed298c5b45ca8611accfdcb7256e713aff8ab057
popty <86563042+popty@users.noreply.github.com>
2022-02-19 20:12:09 +0530

    Time: 2 ms (70.58%), Space: 41.4 MB (19.79%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,27 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x <= 1) return x;        
-        
-        long lo = 0, hi = x/2;
-        long mid;
-        long res = 0;
-        while(lo<hi){
-            
-            mid = lo+(hi-lo)/2;
-            
-            if(mid*mid == x)return (int)mid;
-            
-            if(mid*mid > x ){
-                hi = mid-1;
-            }
-            else if(mid*mid<x){
-                res = mid;
-                lo = mid+1;
-            }
-        }
-        
-        if(lo*lo <= x)return (int)lo;
-        
-        return (int)res;
+       long r = x;
+    while (r*r > x)
+        r = (r + x/r) / 2;
+    return (int)r;  
     }
 }
