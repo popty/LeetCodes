2b60ac3de2e74ad66c6a1799939c95059c3aa7d9
popty <86563042+popty@users.noreply.github.com>
2022-02-19 23:52:45 +0530

    Time: 1 ms (94.71%), Space: 45.7 MB (22.28%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,10 +1,15 @@
 class Solution {
-    public void duplicateZeros(int[] a) {
-         int i = 0, sh = 0;
-  for (i = 0; sh + i < a.length; ++i) sh += a[i] == 0 ? 1 : 0;
-  for (i = i - 1; sh > 0; --i) {
-    if (i + sh < a.length) a[i + sh] = a[i];
-    if (a[i] == 0) a[i + --sh] = a[i];
-  }
+    public void duplicateZeros(int[] arr) {
+        int  count = 0;
+        int n = arr.length;
+        int i = 0;
+        for(i = 0; i+count<arr.length; i++)
+            if(arr[i] == 0) count++;
+        
+        for(i = i-1; count>0; i--){
+            if(i+count < n)arr[i+count] = arr[i];
+            if(arr[i] == 0) arr[i+--count] = arr[i];
+        }
+        
     }
 }
