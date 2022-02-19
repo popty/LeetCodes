6f0fc0981c916017a1fabf48c060495667b4d415
popty <86563042+popty@users.noreply.github.com>
2022-02-19 08:50:22 +0530

    Time: 6 ms (96.02%), Space: 42.2 MB (43.68%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,25 +1,53 @@
 class Solution {
+    int[] lps;
+    
     public int strStr(String haystack, String needle) {
-        if(haystack.length() < needle.length()) return -1;
         
-        if(needle.equals("")) return 0;
-        
+        int m = haystack.length();
         int n = needle.length();
         
-        int index = -1;
-        for(int i = 0; i<haystack.length()-n+1; i++){
-            int k = i;
-            int j;
-            for(j = 0; j<n; j++){
-               if(j == 0)index = i;
-                if(haystack.charAt(k) != needle.charAt(j)){
-                    break;
-                }
-                k++;
+        if(needle.length() == 0) return 0;
+        
+        if(m < n) return -1;
+        
+        preprocessor(needle, n);
+        
+        for(int i = 0, j = 0; i<m; ){
+            if(haystack.charAt(i)== needle.charAt(j)){
+                i++;
+                j++;
             }
-            if(j == n) return index;
+            
+            if(j == n) return i-j;
+        
+            if(i<m && haystack.charAt(i)!= needle.charAt(j)){
+                if(j == 0)i++;
+                else{
+                    j = lps[j-1];
+                }
+            }
+            
         }
         
         return -1;
+        
     }
+    
+    private void preprocessor(String t, int n){
+        this.lps = new int[n];
+        int len = 0;
+        for(int i = 1; i<n;){
+            if(t.charAt(i) == t.charAt(len)){
+               lps[i++] = ++len;
+            }
+            else if(len > 0)
+                len = lps[len-1];
+            else
+                lps[i++]  = 0;
+        }
+        
+        return;
+    }
+    
+    
 }
