51cca28e83ce4545fcefb9075665685f4d109213
popty <86563042+popty@users.noreply.github.com>
2022-02-19 20:20:16 +0530

    Time: 1 ms (99.48%), Space: 39.5 MB (33.00%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -2,7 +2,7 @@ class Solution {
     public int mySqrt(int x) {
        long r = x;
     while (r*r > x)
-        r = (r + x/r) / 2;
-    return (int)r;  
+        r = (r+x/r) /2;
+    return (int)r; 
     }
 }
