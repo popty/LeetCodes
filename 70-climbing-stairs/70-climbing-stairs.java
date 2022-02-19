b7ccc7b9bbbbdf146e0cbb884ab3e01434a8d402
popty <86563042+popty@users.noreply.github.com>
2022-02-19 22:05:01 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (12.29%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
         if(n <=2) return n;
-        int[] ways = new int[n+1];
-        ways[0] = 0;
-        ways[1] = 1;
-        ways[2] = 2;
-        
+        int ways1 = 1, ways2 = 2;
+        int waysRes = 0;
         for(int i = 3; i<=n; i++){
-            ways[i] = ways[i-1]+ways[i-2];
+            waysRes = ways1+ways2;
+            ways1 = ways2;
+            ways2 = waysRes;
         }
         
-        return ways[n];
+        return waysRes;
     }
 }
