c151e3be44006a4731b39bbd6dd964a76f095700
popty <86563042+popty@users.noreply.github.com>
2022-02-20 09:18:40 +0530

    Time: 1 ms (57.54%), Space: 42.4 MB (17.11%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,49 +14,13 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null)return true;
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        List<List<TreeNode>> list = new ArrayList<>();
-        while(!queue.isEmpty()){
-            List<TreeNode> tempList = new ArrayList<>();
-            int len = queue.size();
-            for(int i = 0; i<len; i++){
-                TreeNode temp = queue.poll();
-                tempList.add(temp);
-                if(temp!=null)queue.offer(temp.left);
-                if(temp!=null)queue.offer(temp.right);
-            }
-            list.add(tempList);
-        }
-        
-        for(int i = 0; i<list.size(); i++){
-            List<TreeNode> tempList = list.get(i);
-            int start = 0, end = tempList.size() -1;
-            while(start < end){
-                if(tempList.get(start) == null && tempList.get(end) == null){
-                    start++;
-                    end--;
-                    continue;
-                }
-                else{
-                    if(tempList.get(start) == null || tempList.get(end) == null){
-                        return false;
-                    }
-                    
-                    else if(tempList.get(start).val != tempList.get(end).val) return false;
-                    
-                    else{
-                        start++;
-                        end--;
-                    }
-                }
-            }
-        }
-        
-        return true;
-        
-    }
+   public boolean isSymmetric(TreeNode root) {
+    if(root==null) return true;
+    return isMirror(root.left,root.right);
+}
+public boolean isMirror(TreeNode p, TreeNode q) {
+    if(p==null && q==null) return true;
+    if(p==null || q==null) return false;
+    return (p.val==q.val) && isMirror(p.left,q.right) && isMirror(p.right,q.left);
+}
 }
