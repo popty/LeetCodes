4d765f0cc6179808d32b5c3c2995b4698fa39112
popty <86563042+popty@users.noreply.github.com>
2022-02-21 11:50:35 +0530

    Time: 2 ms (82.16%), Space: 56.7 MB (8.13%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int count = 0;
+        int majority = 0;
+        
+        for(int num: nums){
+            
+            if(count == 0){
+                majority = num;
+                count++;
+            }
+            
+           else if(num == majority){
+                count++;
+            }
+           else count--;
+        }
+        
+        return majority;
+    }
+}
