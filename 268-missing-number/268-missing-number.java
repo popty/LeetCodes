350e46e362d11830ed44226213db0407b7688e04
popty <86563042+popty@users.noreply.github.com>
2022-02-21 11:17:07 +0530

    Time: 1 ms (74.73%), Space: 50.9 MB (29.93%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,22 +1,8 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int max = 0;
-        long actual_sum = 0;
-        for(int num: nums){
-            if(num > max) max = num;
-            actual_sum+=num;
-            
-        }
-        
-        if(max == n-1)return n;
-        
-        long ideal_sum = n*(n-1)/2;
-        
-        if(ideal_sum == actual_sum) return n;
-        
-        actual_sum-=n;
-        
-        return (int)(ideal_sum-actual_sum);
-    }
+    public static int missingNumber(int[] nums) {
+    int sum = nums.length;
+    for (int i = 0; i < nums.length; i++)
+        sum += i - nums[i];
+    return sum;
+}
 }
