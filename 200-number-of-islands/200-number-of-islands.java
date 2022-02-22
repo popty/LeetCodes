0a73e17917e76adec839af5f8c9b3d629ff2cc70
popty <86563042+popty@users.noreply.github.com>
2022-02-22 08:56:28 +0530

    Time: 9 ms (23.91%), Space: 56.3 MB (30.95%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,47 +1,45 @@
 class Solution {
-    char[][] grid;
-    boolean[][] visited;
-    int m, n;
-    int[][] dir ={{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    int numIslands = 0;
-    
-    public boolean isValid(int i, int j){
-        if(i>-1 && j>-1 && i<m && j<n && grid[i][j] != '0' && visited[i][j] == false)return true;
-        
-        return false;
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
     }
-    
-    public void helper(int i, int j){
-        visited[i][j] = true;
-        
-        if(grid[i][j] == '1'){
-            for(int[] d: dir){
-                if(isValid(i+d[0], j+d[1]))
-                    helper(i+d[0], j+d[1]);
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          grid[r][c] = '0'; // mark as visited
+          Queue<Integer> neighbors = new LinkedList<>();
+          neighbors.add(r * nc + c);
+          while (!neighbors.isEmpty()) {
+            int id = neighbors.remove();
+            int row = id / nc;
+            int col = id % nc;
+            if (row - 1 >= 0 && grid[row-1][col] == '1') {
+              neighbors.add((row-1) * nc + col);
+              grid[row-1][col] = '0';
             }
-        }
-        
-        return;
-    }
-    
-    public int numIslands(char[][] grid) {
-        
-        this.grid = grid;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        
-        this.visited = new boolean[m][n];
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(isValid(i, j)){
-                    helper(i, j);
-                    this.numIslands+=1;
-                }
+            if (row + 1 < nr && grid[row+1][col] == '1') {
+              neighbors.add((row+1) * nc + col);
+              grid[row+1][col] = '0';
             }
-           
+            if (col - 1 >= 0 && grid[row][col-1] == '1') {
+              neighbors.add(row * nc + col-1);
+              grid[row][col-1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col+1] == '1') {
+              neighbors.add(row * nc + col+1);
+              grid[row][col+1] = '0';
+            }
+          }
         }
-        
-        return this.numIslands;
-        
+      }
     }
+
+    return num_islands;
+  }
 }
