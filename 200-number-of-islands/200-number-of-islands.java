6f84cb3d892f7883df772f5ef7639b974110a20f
popty <86563042+popty@users.noreply.github.com>
2022-02-22 08:57:19 +0530

    Time: 4 ms (71.45%), Space: 57.3 MB (23.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,45 +1,29 @@
-class Solution {
-  public int numIslands(char[][] grid) {
-    if (grid == null || grid.length == 0) {
-      return 0;
-    }
+public class Solution {
 
-    int nr = grid.length;
-    int nc = grid[0].length;
-    int num_islands = 0;
+private int n;
+private int m;
 
-    for (int r = 0; r < nr; ++r) {
-      for (int c = 0; c < nc; ++c) {
-        if (grid[r][c] == '1') {
-          ++num_islands;
-          grid[r][c] = '0'; // mark as visited
-          Queue<Integer> neighbors = new LinkedList<>();
-          neighbors.add(r * nc + c);
-          while (!neighbors.isEmpty()) {
-            int id = neighbors.remove();
-            int row = id / nc;
-            int col = id % nc;
-            if (row - 1 >= 0 && grid[row-1][col] == '1') {
-              neighbors.add((row-1) * nc + col);
-              grid[row-1][col] = '0';
+public int numIslands(char[][] grid) {
+    int count = 0;
+    n = grid.length;
+    if (n == 0) return 0;
+    m = grid[0].length;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == '1') {
+                DFSMarking(grid, i, j);
+                ++count;
             }
-            if (row + 1 < nr && grid[row+1][col] == '1') {
-              neighbors.add((row+1) * nc + col);
-              grid[row+1][col] = '0';
-            }
-            if (col - 1 >= 0 && grid[row][col-1] == '1') {
-              neighbors.add(row * nc + col-1);
-              grid[row][col-1] = '0';
-            }
-            if (col + 1 < nc && grid[row][col+1] == '1') {
-              neighbors.add(row * nc + col+1);
-              grid[row][col+1] = '0';
-            }
-          }
-        }
-      }
-    }
+    }    
+    return count;
+}
 
-    return num_islands;
-  }
+private void DFSMarking(char[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+    grid[i][j] = '0';
+    DFSMarking(grid, i + 1, j);
+    DFSMarking(grid, i - 1, j);
+    DFSMarking(grid, i, j + 1);
+    DFSMarking(grid, i, j - 1);
+}
 }
