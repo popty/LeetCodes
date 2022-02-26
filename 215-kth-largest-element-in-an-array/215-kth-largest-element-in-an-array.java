7054d85a6a5d480253bd2487ad42329428b7d74f
popty <86563042+popty@users.noreply.github.com>
2022-02-26 22:43:25 +0530

    Time: 7 ms (50.83%), Space: 42.1 MB (63.73%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,12 +1,48 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> n1-n2);
+    int[] nums;
+    int size;
+    
+    private void swap(int a, int b){
+        int temp = this.nums[a];
+        this.nums[a] = this.nums[b];
+        this.nums[b] = temp;
         
-        for(int num: nums){
-            pq.add(num);
-            if(pq.size() > k) pq.poll();
+        return;
+    }
+    
+    private int partition(int start, int end){
+        int pivot = nums[start];
+        int i = start+1;
+        for(int j = start+1; j<=end; j++){
+            if(nums[j] < pivot){
+                swap(i, j);
+                i+=1;
+            }
         }
         
-        return pq.poll();
+        swap(start, i-1);
+        
+        return i-1;
+    }
+    
+    
+    public int quickselect(int start, int end, int targ_ind){
+        if(start == end) return nums[start];
+        
+        int pivot_index = partition(start, end);
+        
+        if(targ_ind == pivot_index) return nums[pivot_index];
+        
+        else if(targ_ind < pivot_index) return quickselect(start, pivot_index-1, targ_ind); 
+        
+        else return quickselect(pivot_index+1, end, targ_ind);
+        
+        
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        this.nums = nums;
+        this.size = nums.length;
+        return quickselect(0, size-1, size-k);
     }
 }
