4ba78f94935b850255ca9276626fdfaf49635ea3
popty <86563042+popty@users.noreply.github.com>
2022-02-27 16:16:56 +0530

    Time: 3 ms (62.49%), Space: 79.9 MB (13.70%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,9 +1,9 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int total_surplus = 0; // it will give us a difference b/w gas & cost
-        int surplus = 0; // our tank
-        int start = 0; // and the index of gas station
+        int total_surplus = 0;
+        int surplus = 0;
+        int start = 0;
         
         for(int i = 0; i < n; i++){
             total_surplus += gas[i]-cost[i];
@@ -12,7 +12,6 @@ class Solution {
                 surplus = 0;
                 start = i+1;
             }
-        
         }
         return (total_surplus < 0) ? -1 : start;
        
