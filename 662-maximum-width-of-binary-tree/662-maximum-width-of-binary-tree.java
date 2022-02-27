1b5dd3fb1284f3b01f6249632c5c4e57e9083af9
popty <86563042+popty@users.noreply.github.com>
2022-02-27 11:42:19 +0530

    Time: 3 ms (51.41%), Space: 43 MB (35.98%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -8,35 +8,19 @@
  * }
  */
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null)
-            return 0;
-
-        // queue of elements [(node, col_index)]
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        Integer maxWidth = 0;
-
-        queue.offer(new Pair<>(root, 1));
-        while (queue.size() > 0) {
-         
-            int l = queue.peek().getValue();
-            int r = l;
-            Integer size = queue.size();
-          
-            for (int i = 0; i < size; ++i) {
-                
-                TreeNode node = queue.peek().getKey();
-                r = queue.peek().getValue();
-                queue.poll();                
-                if (node.left != null)
-                    queue.offer(new Pair<>(node.left, 2 * r));
-                if (node.right != null)
-                    queue.offer(new Pair<>(node.right, 2 * r + 1));
-            }
-
-            maxWidth = Math.max(maxWidth, r - l + 1);
+     public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
+    }
+    
+    public int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end){
+        if(root == null)return 0;
+        if(start.size() == level){
+            start.add(order); end.add(order);
         }
-
-        return maxWidth;
+        else end.set(level, order);
+        int cur = end.get(level) - start.get(level) + 1;
+        int left = dfs(root.left, level + 1, 2*order, start, end);
+        int right = dfs(root.right, level + 1, 2*order + 1, start, end);
+        return Math.max(cur, Math.max(left, right));
     }
 }
