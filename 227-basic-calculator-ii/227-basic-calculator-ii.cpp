03e672787393f41d9a4be96f5d5876f60324f8e3
popty <86563042+popty@users.noreply.github.com>
2022-02-28 16:22:28 +0530

    Time: 13 ms (56.47%), Space: 8.3 MB (65.54%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int ans = 0;
+        istringstream ss("+" + s);
+        char op;
+        int last;
+        int n;
+        while(ss >> op >> n){
+            if(op == '+' || op == '-'){
+                n = op == '+' ? n : -n;
+                ans+=n;
+            }else{
+                n = op == '*' ? last*n : last/n;
+                
+                ans = ans - last + n;
+            }
+            
+            last = n;
+        }
+        
+        return ans;
+        
+        
+    }
+};
