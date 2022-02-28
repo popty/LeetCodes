59cb7603f9d79036a0f41836e508712037c2882e
popty <86563042+popty@users.noreply.github.com>
2022-02-28 23:19:12 +0530

    Time: 3 ms (86.56%), Space: 42.7 MB (27.21%) - LeetHub

--- 772-basic-calculator-iii/772-basic-calculator-iii.java
+++ 772-basic-calculator-iii/772-basic-calculator-iii.java
@@ -1,54 +1,56 @@
 class Solution {
  public int calculate(String s) {
-   Queue<Character> tokens = new ArrayDeque<Character>();
-
-   for(char c : s.toCharArray()){
-       if(c != ' ') tokens.offer(c);
-   }
-
-   tokens.offer('+');
-   return calculate(tokens);
+   Queue<Character> tokens = new LinkedList<>();
+     for(char c: s.toCharArray()){
+         if(c!=' ')tokens.offer(c);
+     }
+     
+     tokens.offer('+');
+     return calculate(tokens);
+     
+     
 }
 
-private int calculate(Queue<Character> tokens){
-
-   char preOp = '+';
-   int num = 0, sum = 0, prev = 0;
-
-   while(!tokens.isEmpty()){
-        char c = tokens.poll();
-
-       while(!tokens.isEmpty() && '0' <= c && c <= '9') {
-            num = num * 10 + c - '0';
-            c = tokens.poll();
-        } if(c=='(') {
-            num = calculate(tokens);
-        }else{
-            switch (preOp){
-                case '+':
-                    sum += prev;
-                    prev = num;
-                    break;
-                case '-':
-                    sum += prev;
-                    prev = -num;
-                    break;
-                case '*':
-                    prev *= num;
-                    break;
-                case '/':
-                    prev /= num;
-                    break;
+    private int calculate(Queue<Character> tokens){
+        char preop = '+';
+        int num = 0, sum = 0, prev = 0;
+        while(!tokens.isEmpty()){
+            
+            char c = tokens.poll();
+           
+            while(!tokens.isEmpty() && c>='0' && c<='9'){
+                num = num*10 + c -'0';
+                c = tokens.poll();
             }
-
-            if(c == ')')
-                break;
-
-            preOp = c;
-            num = 0;
+            
+            if(c == '('){
+                num = calculate(tokens);
+            }else{
+                switch(preop){
+                    case '+':
+                        sum = sum+prev;
+                        prev = num;
+                        break;
+                    case '-':
+                        sum = sum+prev;
+                        prev = -num;
+                        break;
+                    case '*':
+                        prev*=num;
+                        break;
+                    case '/':
+                        prev/=num;
+                        break;
+                }
+                
+                if(c == ')') break;
+                
+                preop = c;
+                num = 0;
+            }
         }
-   }
+        
+        return sum + prev;
 
-   return sum + prev;
-}
+    }
 }
