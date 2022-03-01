10ff384ce5263bf52753ee9127ba9580dbb2fb61
popty <86563042+popty@users.noreply.github.com>
2022-03-01 10:42:19 +0530

    Time: 2 ms (82.30%), Space: 48.3 MB (24.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,11 +1,7 @@
 class Solution {
-    public int[] countBits(int n) {
-        int[] count = new int[n+1];
-       for(int N = 0; N<=n; N++){
-            for(int i = 0; i<32; i++){
-                if(((N>>i) & 1 )> 0)count[N]++;
-            }
-       }
-        return count;        
+    public int[] countBits(int num) {
+    int[] f = new int[num + 1];
+    for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+    return f;           
     }
 }
