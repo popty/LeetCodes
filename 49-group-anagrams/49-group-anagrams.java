31238d65b55678a982989537d448cc23f3279fb4
popty <86563042+popty@users.noreply.github.com>
2022-03-02 10:37:29 +0530

    Time: 16 ms (42.65%), Space: 55.6 MB (39.56%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,20 +1,15 @@
-import java.util.stream.*;
-
 class Solution {
     
-    private String sortChars(String str){
+    private String sortChar(String str){
         char[] charArr = str.toCharArray();
         Arrays.sort(charArr);
         return new String(charArr);
     }
     
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = 
+            Arrays.stream(strs).collect(Collectors.groupingBy(w ->sortChar(w)));
         
-       List<List<String>> list = new ArrayList<>();
-         
-       Map<String, List<String>> map =
-       Arrays.stream(strs).collect(Collectors.groupingBy(w -> sortChars(w)));
-       
-       return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values());
     }
 }
