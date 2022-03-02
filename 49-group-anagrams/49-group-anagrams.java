f6941083ce081362365454deb99d2cde2f5f9860
popty <86563042+popty@users.noreply.github.com>
2022-03-02 10:28:50 +0530

    Time: 14 ms (55.18%), Space: 56.1 MB (27.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -10,15 +10,11 @@ class Solution {
     
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        List<List<String>> list = new ArrayList<>();
-        
+       List<List<String>> list = new ArrayList<>();
+         
        Map<String, List<String>> map =
        Arrays.stream(strs).collect(Collectors.groupingBy(w -> sortChars(w)));
-        
-        for(List<String> tempList : map.values()){
-            list.add(tempList);
-        }
-        
-        return list;
+       
+       return new ArrayList<>(map.values());
     }
 }
