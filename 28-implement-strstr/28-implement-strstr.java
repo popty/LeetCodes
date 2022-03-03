05a20a104de7102a5a156362a13ad85d9c49a640
popty <86563042+popty@users.noreply.github.com>
2022-03-03 22:01:50 +0530

    Time: 13 ms (88.19%), Space: 42.7 MB (50.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,53 +1,45 @@
 class Solution {
-    int[] lps;
-    
     public int strStr(String haystack, String needle) {
         
-        int m = haystack.length();
-        int n = needle.length();
+        int m = haystack.length(), n = needle.length();
+        if(n == 0) return 0;
         
-        if(needle.length() == 0) return 0;
+        int[] lps = helper(needle);
         
-        if(m < n) return -1;
-        
-        preprocessor(needle, n);
-        
-        for(int i = 0, j = 0; i<m; ){
-            if(haystack.charAt(i)== needle.charAt(j)){
+        for(int i = 0, j = 0; i<m;){
+            if(haystack.charAt(i) == needle.charAt(j)){
                 i++;
                 j++;
             }
             
-            if(j == n) return i-j;
-        
-            if(i<m && haystack.charAt(i)!= needle.charAt(j)){
-                if(j == 0)i++;
-                else{
-                    j = lps[j-1];
-                }
+            if(j == n){
+                return i-j;
             }
             
+            if(i < m && haystack.charAt(i) != needle.charAt(j)){
+                if(j == 0) 
+                    i++;
+                else
+                     j = lps[j-1];
+            }
         }
         
         return -1;
-        
     }
     
-    private void preprocessor(String t, int n){
-        this.lps = new int[n];
-        int len = 0;
-        for(int i = 1; i<n;){
-            if(t.charAt(i) == t.charAt(len)){
-               lps[i++] = ++len;
-            }
-            else if(len > 0)
+    private int[] helper(String needle){
+        int n = needle.length();
+        int[] lps = new int[n];
+        for(int i = 1, len = 0; i<n; ){
+            if(needle.charAt(i) == needle.charAt(len)){
+                lps[i++] = ++len;
+            }else if(len != 0){
                 len = lps[len-1];
-            else
-                lps[i++]  = 0;
+            }else{
+                lps[i++] = 0;
+            }
         }
         
-        return;
+        return lps;
     }
-    
-    
 }
