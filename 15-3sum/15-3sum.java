3f769875432191b7565cf82126eed4eca3b3be44
popty <86563042+popty@users.noreply.github.com>
2022-03-03 17:47:24 +0530

    Time: 40 ms (44.37%), Space: 58.2 MB (52.77%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -2,28 +2,22 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-        int n = nums.length;
-        for(int i = 0; i<n-1; i++){
-            if(i==0 || nums[i] != nums[i-1]){
-                int lo = i+1, hi = n-1;
-                int sum = 0-nums[i];
+        for(int i = 0; i<nums.length-2; i++){
+            if(i!=0 && nums[i] == nums[i-1])continue;
+            int lo = i+1, hi = nums.length -1;
+            while(lo < hi){
+              if(nums[i]+nums[lo]+nums[hi] == 0) {
+                  list.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                  while(lo < hi && nums[lo] == nums[lo+1])lo++;
+                  while(lo < hi && nums[hi] == nums[hi-1])hi--;
+                  lo++;
+                  hi--;
+              }
                 
-                while(lo < hi){
-                    if(nums[lo] + nums[hi] == sum){
-                        
-                        list.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        while(lo < hi && nums[lo] == nums[lo+1])lo++;
-                        while(lo<hi && nums[hi] == nums[hi-1])hi--;
-                        lo++;
-                        hi--;
-                    }
-                    
-                    else if(nums[lo] + nums[hi] > sum) hi--;
-                    
-                    else lo++;
-                }
-                
+              else if(nums[i]+nums[lo]+nums[hi] > 0)hi--;
+              else lo++;
             }
+            
         }
         
         return list;
