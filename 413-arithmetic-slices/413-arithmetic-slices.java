1c2451293cfeef15545ab8d4d9f1d77f04a17ddc
popty <86563042+popty@users.noreply.github.com>
2022-03-03 08:57:04 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (23.75%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,14 +1,15 @@
-class Solution {
-    public int numberOfArithmeticSlices(int[] nums) {
-        int count = 0, sum = 0;
-        if(nums.length < 3) return 0;
-        for(int i = 2; i<nums.length; i++){
-            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2])count++;
-            else
-                count = 0;
-            sum+=count;
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int count = 0;
+        for (int s = 0; s < A.length - 2; s++) {
+            int d = A[s + 1] - A[s];
+            for (int e = s + 2; e < A.length; e++) {
+                if (A[e] - A[e - 1] == d)
+                    count++;
+                else
+                    break;
+            }
         }
-        
-        return sum;
+        return count;
     }
 }
