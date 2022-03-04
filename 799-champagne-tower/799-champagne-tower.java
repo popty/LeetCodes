9a08dd0e391da3d9b6c3046fdeaf74cb26b2ee0a
popty <86563042+popty@users.noreply.github.com>
2022-03-04 09:17:56 +0530

    Time: 19 ms (22.83%), Space: 55.8 MB (5.02%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.java
+++ 799-champagne-tower/799-champagne-tower.java
@@ -1,12 +1,10 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        
-        double[][] glasses = new double[102][102];
+        double[][] glasses = new double[101][101];
         glasses[0][0] = (double)poured;
-        
         for(int i = 0; i<=query_row; i++){
-            for(int j = 0; j<=i; j++ ){
-                double q = (glasses[i][j] - 1.0)/2;
+            for(int j = 0; j<=i; j++){
+                double q = (glasses[i][j] - 1.0)/2.0;
                 if(q > 0){
                     glasses[i+1][j] += q;
                     glasses[i+1][j+1] += q;
@@ -15,6 +13,5 @@ class Solution {
         }
         
         return Math.min(1, glasses[query_row][query_glass]);
-        
     }
 }
