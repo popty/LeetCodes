b8a532b64cbf9205e06a4420b82efddb4fdb6f87
popty <86563042+popty@users.noreply.github.com>
2022-03-05 01:35:21 +0530

    Time: 24 ms (73.59%), Space: 64.6 MB (19.91%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -1,37 +1,31 @@
 class Solution {
-    public String getSmallestString(int n, int s) {
-      if(n > s) return new String("Invalid Input"); 
-      
-      if(s > 26*n) return new String("Not Possible");
-      
-      char[] charArr = new char[n];
-      
-      int sum = n;
-      
-      int distance = s-n;
-      
-      for(int i = 0; i<n; i++){
-          charArr[i] = 'a';
-      }
-      
-      for(int i = n-1; i>=0; i--){
-          
-          if(distance == 0){
-              return new String(charArr);
-          }
-          
-          if(distance >= 26){
-              charArr[i] = 'z';
-              distance = distance+1 - 26;
-          }
-          
-          else{
-              //aaa 27  az = 28 , 2
-              charArr[i] = (char)('a'+distance);
-              distance = 0;
-          }
-      }
-      
-      return new String(charArr);
+    public String getSmallestString(int n, int k) {
+        if(n > k) return new String("Invalid Input");
+        
+        if(k > 26*n) return new String("Not Possible");
+        
+        char[] charArr = new char[n];
+        
+        Arrays.fill(charArr, 'a');
+        
+        int distance = k - n;
+        
+        for(int i = n-1; i>=0; i--){
+            
+            if(distance == 0)return new String(charArr);
+            
+            if(distance >= 26){
+                charArr[i] = 'z';
+                distance = distance - 26 + 1;
+            }
+            
+            else{
+                charArr[i] = (char)('a'+distance);
+                distance = 0;
+            }
+        }
+        
+        return new String(charArr);
+        
     }
 }
