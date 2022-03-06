5063d8ebd6da05fa74d5479218666f0354978234
popty <86563042+popty@users.noreply.github.com>
2022-03-06 11:24:19 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (6.08%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        return search(nums, 0, nums.length - 1);
+    
+    private int binary(int[] nums, int start, int end){
+        if(start == end) return start;
+        
+        int mid = start + (end-start)/2;
+        
+        if(nums[mid] > nums[mid+1])
+            return binary(nums, start, mid);            return binary(nums, mid+1, end);
     }
     
-    public int search(int[] nums, int l, int r){
-        if(l == r)return l;
-        
-        int mid =  l+(r-l)/2;
-        if(nums[mid] > nums[mid+1])
-            return search(nums, l, mid);
-        return search(nums, mid+1, r);
+    public int findPeakElement(int[] nums) {
+        return binary(nums, 0, nums.length-1);
     }
 }
