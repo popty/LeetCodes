878eb7a49dd3635d561c8f213cf7eec1d3aa27ab
popty <86563042+popty@users.noreply.github.com>
2022-03-06 22:49:41 +0530

    Time: 8 ms (67.12%), Space: 49.7 MB (29.22%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,15 +1,22 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+      
         Map<Integer, List<Integer>> adjList = new HashMap<>();
+       
         int[] indegrees = new int[numCourses];
+        
         Queue<Integer> bfs = new LinkedList<>();
+        
         int[] result = new int[numCourses];
         
         for(int[] pre : prerequisites){
-            List<Integer> dependencies = 
+            List<Integer> dependents = 
                 adjList.getOrDefault(pre[1], new ArrayList<>());
-            dependencies.add(pre[0]);
-            adjList.put(pre[1], dependencies);
+            
+            dependents.add(pre[0]);
+            
+            adjList.put(pre[1], dependents);
+            
             indegrees[pre[0]]++;
         }
         
@@ -22,15 +29,17 @@ class Solution {
         while(!bfs.isEmpty()){
             int node = bfs.poll();
             result[i++] = node;
-            List<Integer> dependents = adjList.getOrDefault(node, new ArrayList<>());
+            List<Integer> dependents = 
+                adjList.getOrDefault(node, new ArrayList<>());
             for(Integer dependent: dependents){
                 indegrees[dependent]--;
                 if(indegrees[dependent] == 0)bfs.offer(dependent);
             }
         }
         
+        if(i!=numCourses) return new int[0];
         
-        return i==numCourses ? result : new int[0];
+        return result ;
         
         
     }
