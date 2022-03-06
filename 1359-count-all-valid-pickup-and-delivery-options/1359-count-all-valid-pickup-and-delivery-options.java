545d11de8d7f605b829245c5390af668fe039120
popty <86563042+popty@users.noreply.github.com>
2022-03-06 11:01:19 +0530

    Time: 1 ms (54.21%), Space: 41.6 MB (26.64%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,10 +1,13 @@
 class Solution {
     public int countOrders(int n) {
         long ans = 1;
-        int MOD = 1_000_000_007;
-        for(int i = 1; i<=n; i++){
-            ans = (ans * i)%MOD;
-            ans = (ans%MOD * (2*i - 1)%MOD)%MOD;
+        int mod = 1_000_000_007;
+        for(int i = 1; i<=2*n; i++){
+            ans = (ans*i);
+            if(i%2 == 0){
+                ans= ans/2;
+            }
+            ans%=mod;
         }
         
         return (int)ans;
