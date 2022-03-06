d5e065c895fe55d02228880bac2111b5f3a5564b
popty <86563042+popty@users.noreply.github.com>
2022-03-06 15:56:06 +0530

    Time: 19 ms (45.71%), Space: 49.7 MB (19.65%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,9 +2,9 @@ class Solution {
     public int jump(int[] nums) {
         int position = nums.length - 1;
         int steps = 0;
-        while (position != 0) {
-            for (int i = 0; i < position; i++) {
-                if (nums[i] >= position - i) {
+        while(position != 0){
+            for(int i = 0; i<nums.length; i++){
+                if(nums[i]+i>= position){
                     position = i;
                     steps++;
                     break;
