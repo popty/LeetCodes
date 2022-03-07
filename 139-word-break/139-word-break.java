7dcbaaf4d6c404e5218c8a004e055c82ef71c3f1
popty <86563042+popty@users.noreply.github.com>
2022-03-07 11:48:12 +0530

    Time: 17 ms (23.51%), Space: 46.7 MB (33.53%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreakHelper(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);   
-    }
-    
-    private boolean wordBreakHelper(String s, Set<String> wordDict, int start, Boolean[] memo){
-        if(start == s.length()) return true;
-        
-        if(memo[start] != null) return memo[start];
-        
-        for(int i = start+1; i<=s.length(); i++){
-            if(wordDict.contains(s.substring(start, i)) &&
-               wordBreakHelper(s, wordDict, i, memo)){
-                memo[start] = true;
-                return true;
+        if(s.length() == 0) return true;
+        Queue<Integer> bfs = new LinkedList<>();
+        Set<String> dictSet = new HashSet<>(wordDict);
+        bfs.add(0);
+        boolean[] visited = new boolean[s.length()];
+        while(!bfs.isEmpty()){
+            int node = bfs.poll();
+            
+            if(visited[node]) continue;
+            
+            visited[node] = true;
+            
+            for(int i = node+1; i<=s.length(); i++){
+               
+                if(dictSet.contains(s.substring(node, i))){
+                    bfs.offer(i);
+                    if(i == s.length()) return true;
+                }
             }
         }
         
-        return memo[start] = false;
+        return false;
+        
     }
 }
