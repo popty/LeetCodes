9312539138f94fdcf089c2a1c9366479b848ce08
popty <86563042+popty@users.noreply.github.com>
2022-03-08 09:44:52 +0530

    Time: 12 ms (47.34%), Space: 47.4 MB (16.94%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,19 +1,24 @@
 class Solution {
+    
+ 
+    
     public boolean wordBreak(String s, List<String> wordDict) {
         if(s.length() == 0) return true;
         Set<String> dictSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length()+1];
+        Boolean[] dp = new Boolean[s.length()+1];
         Arrays.fill(dp, false);
         dp[0] = true;
+        
         for(int i = 1; i<=s.length(); i++){
-            for(int j = 0; j<i; j++){
+            for(int j = 0; j<=i; j++){
                 if(dp[j] && dictSet.contains(s.substring(j, i))){
                     dp[i] = true;
-                    break;
                 }
             }
         }
         
         return dp[s.length()];
+    
+
     }
 }
