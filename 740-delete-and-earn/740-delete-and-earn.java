be192b01f584a16ee294913fd4b847a96d2a4d8c
popty <86563042+popty@users.noreply.github.com>
2022-03-11 10:47:30 +0530

    Time: 8 ms (33.90%), Space: 42.2 MB (74.06%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,42 +1,24 @@
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    HashMap<Integer, Integer> cache = new HashMap<>();
-    
-    
-    private int helper(int num){
-        if(num == 0) return 0;
-        
-        if(num == 1){
-            return map.getOrDefault(num, 0);
-        }
-        
-        if(cache.containsKey(num)){
-            return cache.get(num);
-        }
-        
-        int gain = map.getOrDefault(num, 0);
-        cache.put(num, Math.max(helper(num-1), gain+helper(num-2)));
-        return cache.get(num);
-    }
-    
     public int deleteAndEarn(int[] nums) {
         
-        int maxNumber = 0;
+        int result = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> maxEarned = new HashMap<>();
+        int max_val = 0;
         for(int num: nums){
-            maxNumber = Math.max(num, maxNumber);
-            int sumSoFar = map.getOrDefault(num, 0);
-            sumSoFar+=num;
-            map.put(num, sumSoFar);
+            map.put(num, map.getOrDefault(num, 0)+num);
+            max_val = Math.max(max_val, num);
         }
         
-        cache.put(0, 0);
-        cache.put(1, map.getOrDefault(1, 0));
-        for(int i = 2; i<=maxNumber; i++){
-            int gain = map.getOrDefault(i, 0);
-            cache.put(i, Math.max(cache.get(i-1), gain+cache.get(i-2)));
+        for(int i = 1; i<=max_val; i++){
+            int maxEarnedHere = Math.max(maxEarned.getOrDefault(i-1, 0), 
+                                     maxEarned.getOrDefault(i-2, 0)+map.getOrDefault(i, 0));
+            maxEarned.put(i, maxEarnedHere);
         }
         
-        return cache.get(maxNumber);
+        
+        return maxEarned.get(max_val);
+        
         
     }
 }
