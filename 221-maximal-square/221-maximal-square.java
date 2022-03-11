df1ee1bf11ce996b7a7def80ac8999288593ff88
popty <86563042+popty@users.noreply.github.com>
2022-03-11 19:48:15 +0530

    Time: 6 ms (81.44%), Space: 58.2 MB (35.88%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        int col = row > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[row+1][col+1];
+        int maxLen = 0;
+        for(int i = 1; i<=row; i++){
+            for(int j = 1; j<=col; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = 
+                        Math.min(Math.min(dp[i-1][j], 
+                                          dp[i][j-1]),
+                                          dp[i-1][j-1]) +1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                    
+                }
+            }
+        }
+        
+        return maxLen*maxLen;
+    }
+}
