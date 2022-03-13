036cb4af37908e33a946046c053fea0ba86de2ea
popty <86563042+popty@users.noreply.github.com>
2022-03-13 09:36:59 +0530

    Time: 6 ms (39.37%), Space: 57.2 MB (40.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,47 @@
-public class Solution {
-
-private int n;
-private int m;
-
-public int numIslands(char[][] grid) {
-    int count = 0;
-    n = grid.length;
-    if (n == 0) return 0;
-    m = grid[0].length;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++)
-            if (grid[i][j] == '1') {
-                DFSMarking(grid, i, j);
-                ++count;
+class Solution {
+    int m, n;
+    char[][] grid;
+    boolean[][] visited;
+    
+    int[][] dirs = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
+    
+    private boolean isValid(int i, int j){
+        if(i >= 0 && i < m && j>=0 && j < n && !visited[i][j] && grid[i][j] == '1') return true;
+        
+        return false;
+    }
+    
+    private void dfs(int i , int j){
+        visited[i][j] = true;
+        
+        for(int[] dir: dirs){
+            int newRow = i+dir[0];
+            int newCol = j+dir[1];
+            if(isValid(newRow, newCol))dfs(newRow, newCol);
+        }
+        
+        return;
+    }
+    
+    
+    
+    public int numIslands(char[][] grid) {
+        this.grid = grid;
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.visited = new boolean[m][n];
+        
+        int totalIslands = 0;
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(isValid(i, j)){
+                    dfs(i,j);
+                    totalIslands++;
+                }
             }
-    }    
-    return count;
-}
-
-private void DFSMarking(char[][] grid, int i, int j) {
-    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-    grid[i][j] = '0';
-    DFSMarking(grid, i + 1, j);
-    DFSMarking(grid, i - 1, j);
-    DFSMarking(grid, i, j + 1);
-    DFSMarking(grid, i, j - 1);
-}
+        }
+        return totalIslands;
+        
+    }
 }
