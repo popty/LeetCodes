617c9f543bfe28094bc9cbadbd9a566bda60cdeb
popty <86563042+popty@users.noreply.github.com>
2022-03-15 19:57:17 +0530

    Time: 1 ms (100.00%), Space: 41.9 MB (81.24%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -4,14 +4,14 @@ class Solution {
             int max = 0;
             for (int num : nums) {
                 buckets[num] += num;
-                
+                max = Math.max(max, num);
             }
             int[] dp = new int[10001];
             dp[0] = buckets[0];
             dp[1] = buckets[1];
-            for (int i = 2; i < buckets.length; i++) {
+            for (int i = 2; i <=max; i++) {
                 dp[i] = Math.max(buckets[i] + dp[i - 2], dp[i - 1]);
             }
-            return dp[10000];
+            return dp[max];
         }
 }
