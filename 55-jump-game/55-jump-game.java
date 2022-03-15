f76f637de4a157a38f65c444f68a575b988d54e9
popty <86563042+popty@users.noreply.github.com>
2022-03-15 19:58:37 +0530

    Time: 2 ms (88.20%), Space: 68.7 MB (17.70%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-     //   boolean[] dp = new boolean[n];
-        //dp[n-1] = true;
         int last_reachable = n-1;
         for(int i = n-2; i>=0; i--){
             if(i+nums[i]>= last_reachable){
                 last_reachable = i;
             }
-        }
-        
+        }        
         return last_reachable == 0;
     }
 }
