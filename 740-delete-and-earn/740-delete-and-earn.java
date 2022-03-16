2ee2dad621f7ca1d30621a6211063258b5ed4aae
popty <86563042+popty@users.noreply.github.com>
2022-03-16 14:54:31 +0530

    Time: 3 ms (86.65%), Space: 44.7 MB (46.37%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,17 +1,21 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-            int[] buckets = new int[10001];
-            int max = 0;
-            for (int num : nums) {
-                buckets[num] += num;
-                max = Math.max(max, num);
-            }
-            int[] dp = new int[10001];
-            dp[0] = buckets[0];
-            dp[1] = buckets[1];
-            for (int i = 2; i <=max; i++) {
-                dp[i] = Math.max(buckets[i] + dp[i - 2], dp[i - 1]);
-            }
-            return dp[max];
+        int[] bucket = new int[10001];
+        int max = 0;
+        
+        if(nums.length == 1) return nums[0];
+        
+        for(int num: nums){
+            bucket[num]+=num;
+            max = Math.max(max, num);
         }
+        
+        int[] dp = new int[max+1];
+        dp[1] = bucket[1];
+        for(int i = 2; i<=max; i++){
+            dp[i] = Math.max(bucket[i]+dp[i-2], dp[i-1]);
+        }
+        
+        return dp[max];
+    }
 }
