9a7daae13bf129763f1ddde1fa07483269d87619
popty <86563042+popty@users.noreply.github.com>
2022-03-16 22:14:07 +0530

    Time: 21 ms (75.19%), Space: 45.7 MB (24.98%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount +1;
-        int[] dp = new int[max];
-        Arrays.fill(dp, max);
-        
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
         dp[0] = 0;
         
-        for(int i = 1; i<=amount; i++){
-            for(int j = 0; j<coins.length; j++){
-                if(coins[j] <= i){
+       for(int j = 0; j<coins.length; j++) {
+            for(int i = 1; i<=amount; i++){
+                if(coins[j] <= i)
                     dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
-                }
             }
         }
         
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount ] > amount ? -1 : dp[amount];
     }
 }
