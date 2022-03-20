637b8195152e12269f261ccaff988712f4f2a134
popty <86563042+popty@users.noreply.github.com>
2022-03-20 10:06:08 +0530

    Time: 11 ms (21.78%), Space: 91.3 MB (43.08%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        Map<Integer, Integer> topFreq = new HashMap<>();
-        Map<Integer, Integer> bottomFreq = new HashMap<>();
+        int[] topFreq = new int[7];
+        int[] bottomFreq = new int[7];
         
         int maxTopFreq = 0;
         int maxBottomFreq = 0;
@@ -12,23 +12,20 @@ class Solution {
         int ans = 0;
         
         for(int i = 0; i<tops.length; i++){
-            int temp = topFreq.getOrDefault(tops[i], 0)+1;
-            if(temp > maxTopFreq){
-                maxTopFreq = temp;
+            
+            topFreq[tops[i]]++;
+            bottomFreq[bottoms[i]]++;
+           
+            if(topFreq[tops[i]] > maxTopFreq){
+                maxTopFreq = topFreq[tops[i]];
                 maxElemTop = tops[i];
             }
-            topFreq.put(tops[i], temp);
             
-            temp = bottomFreq.getOrDefault(bottoms[i], 0)+1;
-            if(temp > maxBottomFreq){
-                maxBottomFreq = temp;
+            if(bottomFreq[bottoms[i]] > maxBottomFreq){
+                maxBottomFreq = bottomFreq[bottoms[i]];
                 maxElemBottom = bottoms[i];
             }
-            bottomFreq.put(bottoms[i], temp);
         }
-        
-        System.out.println(maxElemTop + "  "+ maxElemBottom);
-         System.out.println(maxTopFreq + "  "+ maxBottomFreq);
         
         if(maxTopFreq > maxBottomFreq){
             for(int i = 0; i<tops.length; i+=1){
