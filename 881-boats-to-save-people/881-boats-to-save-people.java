d298e3e24cdb219e8b7a8c8c72f7c2a8eaf2ed6d
popty <86563042+popty@users.noreply.github.com>
2022-03-24 12:35:01 +0530

    Time: 19 ms (78.29%), Space: 66.2 MB (8.94%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -3,20 +3,16 @@ class Solution {
         Arrays.sort(people);
         int boats = 0;
         int i = 0, j = people.length-1;
-        int count = people.length;
-        while(count > 0){
+        while(i <= j){
             if(i!=j && (people[i] + people[j] <= limit)){
                 i++;
                 j--;
-                count-=2;
                 boats++;
             }else if(people[j] <= limit){
                 j--;
                 boats++;
-                count-=1;
             }else if(people[i] > limit) return -1;
         }
-        
         return boats;
     }
 }
